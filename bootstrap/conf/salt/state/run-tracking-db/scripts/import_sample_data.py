e93f991c496cf99979250bb4a6a11a98f7228261
Sergei Iakhnin <llevar@gmail.com>
2015-09-24 16:14:10 +0200

    Swallow the error if table already exists.

--- bootstrap/conf/salt/state/run-tracking-db/scripts/import_sample_data.py
+++ bootstrap/conf/salt/state/run-tracking-db/scripts/import_sample_data.py
@@ -8,4 +8,10 @@ df.columns = [c.lower() for c in df.columns]
 from sqlalchemy import create_engine
 engine = create_engine('postgresql://localhost:5432/germline_genotype_tracking')
 
-df.to_sql("pcawg_samples", engine)
+try:
+    df.to_sql("pcawg_samples", engine)
+except ValueError as e:
+    if e.strerror != "Table 'pcawg_samples' already exists":
+        print e.strerror
+        exit(1)
+        
