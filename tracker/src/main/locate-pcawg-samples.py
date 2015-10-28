3108fbab2ce3794502ae634dd1398767f41faeee
Sergei Iakhnin <llevar@gmail.com>
2015-10-28 11:48:24 +0100

    Script to crawl GNOS filesystem and record the location of samples tracked by the run tracking db.

--- tracker/src/main/locate-pcawg-samples.py
+++ tracker/src/main/locate-pcawg-samples.py
@@ -2,9 +2,11 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 import os.path
+import datetime
 
 base_path = "/gnosdata/data/"
 
+#Connect to the database and reflect the schema into python objects
 Base = automap_base()
 engine = create_engine('postgresql://localhost:5432/germline_genotype_tracking')
 Base.prepare(engine, reflect=True)
@@ -14,27 +16,46 @@ SampleLocations = Base.classes.sample_locations
 
 session = Session(engine)
 
-my_samples = []
+#Create a dict mapping donor_index to sample_location
+sample_locations = {el.donor_index: el for el in session.query(SampleLocations)}
+
 for sample in session.query(PCAWGSamples):
-    my_samples.append(sample)
+    print "Processing Donor #: " + str(sample.index) + " Id: " + sample.submitter_donor_id
     
+    #File location of the tumor BAM file
     tumor_directory = sample.tumor_wgs_alignment_gnos_id
     tumor_filename = sample.tumor_wgs_alignment_bam_file_name
     tumor_full_path = base_path + tumor_directory + "/" + tumor_filename
     
+    #File location of the normal BAM file
     normal_directory = sample.normal_wgs_alignment_gnos_id
     normal_filename = sample.normal_wgs_alignment_bam_file_name
     normal_full_path = base_path + normal_directory + "/" + normal_filename
     
-    this_sample_location = SampleLocations()
-    this_sample_location.donor_index = sample.index
+    #If a sample_location record for this donor already exists, work with it
+    this_sample_location = sample_locations.get(sample.index)
     
-    if isfile(normal_full_path):
+    #If sample_location for this donor does not yet exist, create it.
+    if not this_sample_location:    
+        this_sample_location = SampleLocations()
+        this_sample_location.donor_index = sample.index
+    
+    is_found = False
+    
+    #If file path to the normal BAM file exists, record it
+    if os.path.isfile(normal_full_path):
         this_sample_location.normal_sample_location = normal_full_path
+        is_found = True
     
-    if isfile(tumor_full_path):
+    #If file path to the tumor BAM file exists, record it
+    if os.path.isfile(tumor_full_path):
         this_sample_location.tumor_sample_location = tumor_full_path
+        is_found = True
     
-    session.add(this_sample_location)
-    session.commit()
-    print my_data.__dict__
+    #If at least one file path (tumor or normal) exists, persist the sample_locaion record
+    if is_found:
+        print "BAM files found for Donor: " + sample.submitter_donor_id
+        this_sample_location.last_updated = datetime.datetime.now()
+        session.add(this_sample_location)
+        session.commit()
+ 
