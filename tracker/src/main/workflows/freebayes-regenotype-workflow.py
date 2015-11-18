fb6c16a3c130331e8f2f19a90e5c5843b8b7e4d2
Sergei Iakhnin <llevar@gmail.com>
2015-11-18 15:31:12 +0100

    Fix datetime call in workflow start date.

--- tracker/src/main/workflows/freebayes-regenotype-workflow.py
+++ tracker/src/main/workflows/freebayes-regenotype-workflow.py
@@ -88,7 +88,7 @@ def run_freebayes(**kwargs):
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2020, 11, 9),
+    'start_date': datetime.datetime(2020, 11, 9),
     'email': ['airflow@airflow.com'],
     'email_on_failure': False,
     'email_on_retry': False,
