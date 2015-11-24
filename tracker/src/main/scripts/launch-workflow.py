b0e53f9fa502e502861646f4cab0f1ba960b9271
Sergei Iakhnin <llevar@gmail.com>
2015-11-24 14:39:31 +0100

    Script to launch workflows en masse.

---
+++ tracker/src/main/scripts/launch-workflow.py
@@ -0,0 +1,21 @@
+import os
+import uuid
+from time import sleep
+
+if len(sys.argv) != 3:
+    print "Wrong number of args"
+    exit(1)
+    
+workflow_name = sys.argv[1]
+num_runs = sys.argv[2]
+
+for this_run in range(num_runs):
+    
+    run_uuid = uuid.uuid4()
+    
+    launch_command = "airflow trigger_dag -r " + run_uuid + " " + workflow_name
+    print("Launching workflow with command: " + launch_command)
+    os.system(launch_command)
+    print("Workflow %s launched.", run_uuid)
+    sleep(0.5)
+    
