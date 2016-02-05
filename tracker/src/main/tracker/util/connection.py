e479876274a172d3b71ab075e646200b9ab20d4f
Sergei Iakhnin <llevar@gmail.com>
2016-02-05 15:32:00 +0100

    Disable connection pooling.

--- tracker/src/main/tracker/util/connection.py
+++ tracker/src/main/tracker/util/connection.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy.orm.scoping import scoped_session
+from sqlalchemy.pool import NullPool
 
 DB_URL = os.environ.get('DB_URL')
 
@@ -11,7 +12,7 @@ if not DB_URL:
     raise ValueError("DB_URL not present in the environment")
 
 Base = automap_base()
-engine = create_engine(DB_URL, pool_size=8, max_overflow=2)
+engine = create_engine(DB_URL, poolclass=NullPool)
 Base.prepare(engine, reflect=True)
 
 session_factory  = sessionmaker(bind=engine, expire_on_commit=False)
