20c9a1275a14ecb0d7ab87ba2133877956ed7de5
Sergei Iakhnin <llevar@gmail.com>
2016-02-06 10:44:59 +0100

    Implementation of delly workflow for deletion regenotyping.

--- tracker/src/main/workflows/delly-workflow/delly-workflow.py
+++ tracker/src/main/workflows/delly-workflow/delly-workflow.py
@@ -10,48 +10,42 @@ import tracker.model
 from tracker.model.analysis_run import *
 from tracker.util.workflow_common import *
 
-def run_freebayes(**kwargs):
+def run_delly(**kwargs):
     
     config = get_config(kwargs)
     sample = get_sample(kwargs)
     
-    contig_name = kwargs["contig_name"]
-    contig_whitelist = config.get("contig_whitelist")
+    sample_id = sample["sample_id"]
+    sample_location = sample["sample_location"]
     
-    
-    if not contig_whitelist or contig_name in contig_whitelist:
-    
-        sample_id = sample["sample_id"]
-        sample_location = sample["sample_location"]
-        
-        result_path_prefix = config["results_local_path"] + sample_id
-    
-        if (not os.path.isdir(result_path_prefix)):
-            logger.info("Results directory {} not present, creating.".format(result_path_prefix))
-            os.makedirs(result_path_prefix)
-    
-        result_filename =  "{}/{}_{}.vcf".format(result_path_prefix, sample_id, contig_name)
-            
-        freebayes_path = config["freebayes"]["path"]
-        reference_location = config["reference_location"]
-        variants_location = config["variants_location"]
-        
-        freebayes_command = "{} -r {} -f {} -@ {} -l {} > {}".\
-            format(freebayes_path, \
-                 contig_name, \
-                 reference_location, \
-                 variants_location[contig_name], \
-                 sample_location, \
-                 result_filename)
-            
-        call_command(freebayes_command, "freebayes")
-    
-        compressed_sample_filename = compress_sample(result_filename, config)
-        generate_tabix(compressed_sample_filename, config)
-        copy_result(compressed_sample_filename, sample_id, config)
-    else:
-        logger.info("Contig {} is not in the contig whitelist, skipping.".format(contig_name))
+    result_path_prefix = config["results_local_path"] + sample_id
 
+    if (not os.path.isdir(result_path_prefix)):
+        logger.info("Results directory {} not present, creating.".format(result_path_prefix))
+        os.makedirs(result_path_prefix)
+
+    delly_path = config["delly"]["path"]
+    reference_location = config["reference_location"]
+    variants_location = config["variants_location"]
+    variants_type = config["variants_type"]
+    exclude_template_path = config["delly"]["exclude_template_path"]
+    
+    result_filename = "{}/{}_{}.vcf.gz".format(result_path_prefix, sample_id, variants_type)
+        
+    delly_command = "{} -t {} -g {} -v {} -o {} -x {} {}".\
+        format(delly_path, \
+             variants_type, \
+             reference_location, \
+             variants_location, \
+             result_filename,
+             exclude_template_path, \
+             sample_location)
+        
+    call_command(delly_command, "delly")
+
+    generate_tabix(result_filename, config)
+    copy_result(result_filename, sample_id, config)
+    
 
 default_args = {
     'owner': 'airflow',
@@ -83,22 +77,20 @@ validate_sample_task = PythonOperator(
 
 validate_sample_task.set_upstream(start_analysis_run_task)
 
+delly_task = PythonOperator(
+    task_id="delly_genotype",
+    python_callable=run_delly,
+    provide_context=True,
+    dag=dag)
+
+delly_task.set_upstream(validate_sample_task)
+
 complete_analysis_run_task = PythonOperator(
     task_id="complete_analysis_run",
     python_callable=complete_analysis_run,
     provide_context=True,
     dag=dag)
     
-for contig_name in contig_names:
-    freebayes_task = PythonOperator(
-        task_id="freebayes_" + contig_name,
-        python_callable=run_freebayes,
-        op_kwargs={"contig_name": contig_name},
-        provide_context=True,
-        dag=dag)
-
-    freebayes_task.set_upstream(validate_sample_task)
-
-    complete_analysis_run_task.set_upstream(freebayes_task)
+complete_analysis_run_task.set_upstream(delly_task)
 
 
