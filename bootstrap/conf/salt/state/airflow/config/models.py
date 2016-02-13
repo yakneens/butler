5c712d3d0a8b2ff158e7e58b18b8ec14295f8c13
Sergei Iakhnin <llevar@gmail.com>
2016-02-13 23:01:35 +0100

    Revert

--- bootstrap/conf/salt/state/airflow/config/models.py
+++ bootstrap/conf/salt/state/airflow/config/models.py
@@ -643,7 +643,6 @@ class TaskInstance(Base):
             session.commit()
             session.close()
             settings.engine.dispose()
-        settings.engine.dispose()
         return state
 
     def error(self, main_session=None):
@@ -656,8 +655,6 @@ class TaskInstance(Base):
         session.merge(self)
         session.commit()
         session.close()
-        settings.engine.dispose()
-        
 
     def refresh_from_db(self, main_session=None):
         """
@@ -679,8 +676,7 @@ class TaskInstance(Base):
         if not main_session:
             session.commit()
             session.close()
-            
-        settings.engine.dispose()
+            settings.engine.dispose()
 
     @property
     def key(self):
@@ -752,8 +748,7 @@ class TaskInstance(Base):
         if not main_session:
             session.commit()
             session.close()
-        
-        settings.engine.dispose()
+            settings.engine.dispose()
         return count == len(task._downstream_list)
 
     def are_dependencies_met(
@@ -786,14 +781,12 @@ class TaskInstance(Base):
                 TI.state == State.SUCCESS,
             ).first()
             if not previous_ti:
-                settings.engine.dispose()
                 return False
 
             # Applying wait_for_downstream
             previous_ti.task = self.task
             if task.wait_for_downstream and not \
                     previous_ti.are_dependents_done(session):
-                settings.engine.dispose()
                 return False
 
         # Checking that all upstream dependencies have succeeded
@@ -839,29 +832,24 @@ class TaskInstance(Base):
                     session.merge(self)
 
             if task.trigger_rule == TR.ONE_SUCCESS and successes > 0:
-                settings.engine.dispose()
                 return True
             elif (task.trigger_rule == TR.ONE_FAILED and
                   (failed + upstream_failed) > 0):
-                settings.engine.dispose()
                 return True
             elif (task.trigger_rule == TR.ALL_SUCCESS and
                   successes == len(task._upstream_list)):
-                settings.engine.dispose()
                 return True
             elif (task.trigger_rule == TR.ALL_FAILED and
                   failed + upstream_failed == len(task._upstream_list)):
-                settings.engine.dispose()
                 return True
             elif (task.trigger_rule == TR.ALL_DONE and
                   done == len(task._upstream_list)):
-                settings.engine.dispose()
                 return True
 
         if not main_session:
             session.commit()
             session.close()
-        settings.engine.dispose()
+            settings.engine.dispose()
         return False
 
     def __repr__(self):
@@ -1092,8 +1080,6 @@ class TaskInstance(Base):
         if not test_mode:
             session.merge(self)
         session.commit()
-        session.close()
-        settings.engine.dispose()
         logging.error(str(error))
 
     @provide_session
@@ -1131,8 +1117,6 @@ class TaskInstance(Base):
         if task.params:
             params.update(task.params)
 
-        settings.engine.dispose()
-        
         return {
             'dag': task.dag,
             'ds': ds,
