b452eb4d2b308e0d475b3afae13c3587e97bde6e
Sergei Iakhnin <llevar@gmail.com>
2017-10-16 22:01:14 +0200

    Add mutation queries for all objects

--- track/tracker/graphql/schema.py
+++ track/tracker/graphql/schema.py
@@ -1,73 +1,134 @@
 import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyConnectionField, SQLAlchemyObjectType
-import tracker.model.workflow
+import tracker
+from dateutil import parser
 
-from tracker.model.analysis import Analysis
-from tracker.model.workflow import Workflow
-from tracker.model.analysis_run import AnalysisRun
-from tracker.model.configuration import Configuration
+from tracker.model.analysis import Analysis as AnalysisModel
+from tracker.model.workflow import Workflow as WorkflowModel
+from tracker.model.analysis_run import AnalysisRun as AnalysisRunModel
+from tracker.model.configuration import Configuration as ConfigurationModel
 import json
 
-class WorkflowQ(SQLAlchemyObjectType):
+class Workflow(SQLAlchemyObjectType):
 
     class Meta:
-        model = Workflow
+        model = WorkflowModel
+        interfaces = (relay.Node, )
+        
+class Analysis(SQLAlchemyObjectType):
+
+    class Meta:
+        model = AnalysisModel
         interfaces = (relay.Node, )
 
 
-class createWorkflow(graphene.Mutation):
-    class Arguments:
-        workflowName = graphene.String()
-        workflowVersion = graphene.String()
-        configId = graphene.String()
+class AnalysisRun(SQLAlchemyObjectType):
+
+    class Meta:
+        model = AnalysisRunModel
+        interfaces = (relay.Node, )
+        
+class Configuration(SQLAlchemyObjectType):
+
+    class Meta:
+        model = ConfigurationModel
+        interfaces = (relay.Node, )
+
+class ConfigInput(graphene.InputObjectType):
+    config_id = graphene.ID(required=True)
+    config = graphene.String(required=True)
+
+class CreateConfiguration(relay.ClientIDMutation):
+    class Input:
+        my_config = ConfigInput()
         
     ok = graphene.Boolean()
-    workflow = graphene.Field(WorkflowQ)
+    config = graphene.Field(Configuration)
 
     @classmethod
-    def mutate(cls, _, args, context, info):
-        workflow = create_workflow(args.get("workflowName"), args.get("workflowVersion"), args.get("configId"))
+    def mutate_and_get_payload(cls, root, info, my_config, client_mutation_id=None):
+        new_config = tracker.model.configuration.create_configuration(my_config.config_id, my_config.config)
         ok = True
-        return createWorkflow(workflow=workflow, ok=ok)
+        return CreateConfiguration(config=new_config, ok=ok)
+
+class WorkflowInput(graphene.InputObjectType):
+    workflow_name = graphene.String()
+    workflow_version = graphene.String()
+    config_id = graphene.ID(required=True)
 
 
-class AnalysisQ(SQLAlchemyObjectType):
-
-    class Meta:
-        model = Analysis
-        interfaces = (relay.Node, )
-
-
-class AnalysisRunQ(SQLAlchemyObjectType):
-
-    class Meta:
-        model = AnalysisRun
-        interfaces = (relay.Node, )
+class CreateWorkflow(relay.ClientIDMutation):
+    class Input:
+        my_workflow = WorkflowInput()
         
-class ConfigurationQ(SQLAlchemyObjectType):
+    ok = graphene.Boolean()
+    workflow = graphene.Field(Workflow)
+    
+    @classmethod
+    def mutate_and_get_payload(cls, root, info, my_workflow, client_mutation_id=None):
+        new_workflow = tracker.model.workflow.create_workflow(my_workflow.workflow_name, my_workflow.workflow_version, my_workflow.config_id)
+        ok = True
+        return CreateWorkflow(workflow=new_workflow, ok=ok)
 
-    class Meta:
-        model = Configuration
-        interfaces = (relay.Node, )
+class AnalysisInput(graphene.InputObjectType):
+    analysis_name = graphene.String()
+    start_date = graphene.String()
+    config_id = graphene.ID(required=True)
+
+class CreateAnalysis(relay.ClientIDMutation):
+    class Input:
+        my_analysis = AnalysisInput()
+
+        
+    ok = graphene.Boolean()
+    analysis = graphene.Field(Analysis)
+    
+    @classmethod
+    def mutate_and_get_payload(cls, root, info, my_analysis, client_mutation_id=None):
+        new_analysis = tracker.model.analysis.create_analysis(my_analysis.analysis_name, parser.parse(my_analysis.start_date), my_analysis.config_id)
+        ok = True
+        return CreateAnalysis(analysis=new_analysis, ok=ok)
+    
+class AnalysisRunInput(graphene.InputObjectType):
+    workflow_id = graphene.Int(required=True)
+    analysis_id = graphene.Int(required=True)
+    config_id = graphene.ID(required=True)
+
+class CreateAnalysisRun(relay.ClientIDMutation):
+    class Input:
+        my_analysis_run = AnalysisRunInput()
+
+        
+    ok = graphene.Boolean()
+    analysis_run = graphene.Field(AnalysisRun)
+    
+    @classmethod
+    def mutate_and_get_payload(cls, root, info, my_analysis_run, client_mutation_id=None):
+        new_analysis_run = tracker.model.analysis_run.create_analysis_run(my_analysis_run.analysis_id, my_analysis_run.config_id, my_analysis_run.workflow_id)
+        ok = True
+        return CreateAnalysisRun(analysis_run=new_analysis_run, ok=ok)
 
 
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
-    all_workflows = SQLAlchemyConnectionField(WorkflowQ)
-    all_analyses = SQLAlchemyConnectionField(AnalysisQ)
-    all_analysis_runs = SQLAlchemyConnectionField(AnalysisRunQ)
+    all_workflows = SQLAlchemyConnectionField(Workflow)
+    all_analyses = SQLAlchemyConnectionField(Analysis)
+    all_analysis_runs = SQLAlchemyConnectionField(AnalysisRun)
     
-    workflow = graphene.Field(WorkflowQ)
-    analysis = graphene.Field(AnalysisQ)
-    analysis_run = graphene.Field(AnalysisRunQ)
-    configuration = graphene.Field(ConfigurationQ)
+    workflow = graphene.Field(Workflow)
+    analysis = graphene.Field(Analysis)
+    analysis_run = graphene.Field(AnalysisRun)
+    configuration = graphene.Field(Configuration)
 
 class MyMutations(graphene.ObjectType):
-    create_workflow = createWorkflow.Field()
+    create_config  = CreateConfiguration.Field()
+    create_workflow = CreateWorkflow.Field()
+    create_analysis = CreateAnalysis.Field()
+    create_analysis_run = CreateAnalysisRun.Field()
 
 
-schema = graphene.Schema(query=Query, types=[WorkflowQ, AnalysisQ, AnalysisRunQ, ConfigurationQ], mutation=MyMutations)
+schema = graphene.Schema(query=Query, types=[Workflow, Analysis, AnalysisRun, Configuration], mutation=MyMutations)
 
 
 from graphene.test import Client
--- track/tracker/graphql/test.py
+++ track/tracker/graphql/test.py
@@ -1,13 +1,84 @@
 from graphene.test import Client
-
+from schema import schema
+from examples.complex_example import query
 
 client = Client(schema)
 
-query = '''
-    query TestQuery {
+add_workflow_query = '''
+    mutation addWorkflow($workflow: WorkflowInput){
+      createWorkflow(input:{myWorkflow:$workflow}){
         workflow {
-            name
+          workflowName
+          workflowId
+          workflowVersion
+          configId
+        }
+      }
+    }
+    '''
+    
+add_config_query = '''
+    mutation addConfig($config: ConfigInput){
+      createConfig(input:{myConfig: $config}){
+        config {
+          configId
+          config
+        }
+        ok
+      }
+    }
+    '''
+
+add_analysis_query = '''
+    mutation addAnalysis($analysis: AnalysisInput){
+      createAnalysis(input:{myAnalysis: $analysis}){
+        analysis{
+          analysisId
+          analysisName
+          startDate
+          configId
+        }
+        ok
+      }
+    }
+    '''
+    
+add_analysis_run_query = '''
+    mutation addAnalysisRun($analysisRun: AnalysisRunInput){
+      createAnalysisRun(input: {myAnalysisRun: $analysisRun}){
+        analysisRun{
+          analysisId
+          workflowId
+          analysisRunId
+          configId
+        }
+      }
+    }
+    '''
+query_list = [add_config_query, add_workflow_query, add_analysis_query, add_analysis_run_query]
+
+variable_values = '''
+    {
+        "workflow": {
+            "workflowName":"QWERA",
+            "workflowVersion":"asdasd",
+            "configId":"e08df590-1b28-440d-84ab-be14266ada93"
+        },
+        "config": {
+            "configId":"e08df590-1b28-440d-84ab-be14266ada93",
+            "config": "{\"blah\": \"blah\"}"
+        },
+        "analysis": {
+            "analysisName": "My analysis",
+            "startDate": "10-15-2017",
+            "configId": "e08df590-1b28-440d-84ab-be14266ada93"
+        },
+        "analysisRun": {
+            "workflowId": 1,
+            "analysisId": 1,
+            "configId": "e08df590-1b28-440d-84ab-be14266ada93"
         }
     }
     '''
-client.execute(query)
+for my_query in query_list:
+    client.execute(my_query, variable_values=variable_values)
