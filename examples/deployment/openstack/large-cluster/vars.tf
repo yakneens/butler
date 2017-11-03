2ff051ac04b9ad7cecff4156fcfbc12c65ca0de9
Sergei Iakhnin <llevar@gmail.com>
2017-11-03 16:14:03 +0100

    Get rid of project-specific values

--- examples/deployment/openstack/large-cluster/vars.tf
+++ examples/deployment/openstack/large-cluster/vars.tf
@@ -6,14 +6,14 @@ variable "password" {
 	default="CHANGE_ME"
 }
 variable "tenant_name" {
-	default="Pan-Prostate"
+	default=""
 }
 variable "auth_url" {
 	
 }
 
 variable "key_pair" {
-	default="sergei"
+	default=""
 }
 
 
@@ -26,11 +26,11 @@ variable "bastion_host" {
 }
 
 variable "bastion_user" {
-	default = "iakhnin"
+	default = ""
 }
 
 variable "image_id" {
-	default = "7457dc57-9765-4293-ab1b-8e03d748485f"
+	default = ""
 }
 
 variable "user" {
@@ -42,22 +42,17 @@ variable "key_file" {
 }
 
 variable "network_name" {
-	default = "Pan-Prostate_private"
+	default = ""
 }
 
 variable "main_network_id" {
-	default="d506eaf8-88b5-43c3-a751-a198672017e6"
+	default=""
 }
 
-variable "pan_prostate_network_id" {
-	default="eb60b9be-0d31-41f0-b5d3-f6546cb13a67"
-}
-variable "gnos_network_id" {
-	default="ee244fc2-9540-4523-a642-53d1fad0fb53"
 }
 
 variable "floatingip_pool" {
-	default = "ext-net"
+	default = ""
 }
 
 variable "worker_count" {
@@ -85,5 +80,5 @@ variable "tracker-flavor" {
 }
 
 variable "main-security-group-id" {
-	default="Pan-Prostate-Internal"
+	default=""
 }
